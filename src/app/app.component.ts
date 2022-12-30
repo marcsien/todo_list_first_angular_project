4355e0af418738fbc659fb6dc31944bc112b52dc
marcsien <marcsien@gmail.com>
2022-12-30 08:13:35 +0100

    Adding tasks with click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Task } from './task';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  taskName!: string;
 config: {[key: string]: string} | null = null;
 tasks: Task[] = [
   {
@@ -36,4 +37,23 @@ constructor(){
   }
   }, 500)
 }
+
+clearTasks(){
+  this.tasks = [];
+}
+
+onKeyUp(event: KeyboardEvent){
+  const target = event.target as HTMLInputElement;
+  this.taskName = target.value;
+}
+
+createTask(){
+  const task: Task = {
+    name: this.taskName,
+    deadline: '2022-02-02',
+    done: false,
+  };
+  this.tasks.push(task);
+}
+
 }
